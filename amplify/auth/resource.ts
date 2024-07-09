9395648558d3808fc0783331f76cb7762ef100a3
Manish N Devadiga <gdevadiga109@gmail.com>
2024-07-09 21:14:45 +0530

    added authenticator

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,3 +9,31 @@ export const auth = defineAuth({
     email: true,
   },
 });
+
+
+// To include other authentication providers such as Google or Facebook, or phonenumber you need
+//  to enable and configure them in your defineAuth configuration. Here’s an example
+
+
+// import { defineAuth } from "@aws-amplify/backend";
+
+// /**
+//  * Define and configure your auth resource
+//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+//  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//     phoneNumber: true,
+//     socialProviders: {
+//       google: {
+//         clientId: 'your-google-client-id',
+//         clientSecret: 'your-google-client-secret',
+//       },
+//       facebook: {
+//         clientId: 'your-facebook-client-id',
+//         clientSecret: 'your-facebook-client-secret',
+//       },
+//     },
+//   },
+// });
